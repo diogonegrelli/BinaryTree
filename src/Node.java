2591a5863d4b14192fb603f0e6e570aff4164cfd
diogonegrelli <diogonegrelli@outlook.com>
2023-09-21 10:35:10 -0300

    Commit

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Node {
+public class Node {
+    int data;
+    Node right;
+    Node left;
+
+    public Node(int data, Node right, Node left) {
+        this.data = data;
+        this.right= right;
+        this.left = left;
+    }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Tree {
+public class Tree {
+    Node root;
+
+    public int insertValue(int data){
+        if (root == null){
+            Node root = new Node( data; )
+        }
+        return data;
+    }
+
+    public int searchValue(int value){
+        return value;
+    };
+
+    public int removeValue(int value){
+        return value;
+    }
+
+
+    public void printTree(){
+
+    }
 }
